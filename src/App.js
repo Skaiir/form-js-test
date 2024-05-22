8fe5bad772db4a5111a4e51d22df613b0905db79
Bogdan Oleinikov <b.oleinikov@softlist.ua>
2024-05-22 17:17:09 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import { FormEditor, FormPlayground } from "@bpmn-io/form-js";
 import './App.css';
+import "@bpmn-io/form-js/dist/assets/form-js.css";
+import "@bpmn-io/form-js/dist/assets/form-js-editor.css";
+import "@bpmn-io/form-js/dist/assets/form-js-playground.css";
+import schema from "./empty.json";
+import PropertiesPanelExtension from './propertiesPanel'
+import RangeExtension from './components/Range'
+
+export const JSONToFile = (obj, filename) => {
+  const blob = new Blob([JSON.stringify(obj, null, 2)], {
+    type: 'application/json',
+  });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = `${filename}.json`;
+  a.click();
+  URL.revokeObjectURL(url);
+};
+
 
 function App() {
+
+  const init = async () => {
+    let el = document.querySelector("#form");
+    if (el !== null) {
+      const form = new FormEditor({
+        container: document.querySelector("#form"),
+        // schema: schema,
+        data: {},
+
+        // load rendering extension
+        additionalModules: [
+          RangeExtension,
+          PropertiesPanelExtension
+        ],
+
+        // load properties panel extension
+        // editorAdditionalModules: [PropertiesPanelExtension],
+      });
+      await form.importSchema(schema);
+      // console.log("form properties", form);
+      const btn = document.getElementById("download-button");
+
+      btn?.addEventListener("click", () => {
+        const schema = form.getSchema();
+        JSONToFile(schema, "customSchema");
+      });
+    }
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="form"></div>
+      {/* <button id="download-button">Download schema</button> */}
     </div>
   );
 }
